16e141cf5e1e57c38bf289c957488e59bb7b8e2e
Alli-anne <145496176+Alli-anne@users.noreply.github.com>
2025-10-10 12:55:25 -0600

    swagger updates

--- swagger.js
+++ swagger.js
@@ -10,7 +10,8 @@ const doc = {
 };
 
 const outputFile = './swagger.json';
-const routes = ['./routes/index.js'];
+const routes = ['./routes/index.js', './routes/contact.js'];
+
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
